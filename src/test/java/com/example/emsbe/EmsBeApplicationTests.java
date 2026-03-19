c37975dfe0b2230a5b46d4ec010275886fc043d1
Pasindu Yapa <lolmixage03@gmail.com>
2026-03-19 23:28:58 +0530

    Initial commit

---
+++ src/test/java/com/example/emsbe/EmsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emsbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
